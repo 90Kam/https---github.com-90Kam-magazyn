31e3b360ceaab098dd6768f1a0304913dbf69461
Kamil Gierczycki <k.gierczycki@vrtechnology.pl>
2023-02-02 14:02:10 +0100

    add and edit employee

---
+++ locators/locators.py
@@ -0,0 +1,12 @@
+pracownicy_button = "//span[@class='toggle-text'][normalize-space()='Pracownicy']" # XPATH
+add_employee_button = "//button[@type='button']" # XPATH
+new_employee_name_input = "name" # NAME
+new_employee_lastname_input = "lastName" # NAME
+submit_adding_new_employee_button = "//button[text()='Dodaj']" # XPATH
+filter_employee_button = "(//button[contains(@class,'MuiButtonBase-root MuiIconButton-root')])[2]" # XPATH
+filter_name_input = "(//input[contains(@class,'MuiInputBase-input MuiInput-input')])[2]" # XPATH
+filter_lastname_input = "(//input[contains(@class,'MuiInputBase-input MuiInput-input')])[3]" # XPATH
+found_employee_name = "(//td[contains(@class,'MuiTableCell-root MuiTableCell-body')])[2]" # XPATH
+found_employee_lastname = "(//td[contains(@class,'MuiTableCell-root MuiTableCell-body')])[3]" # XPATH
+edit_employee_button = "//button[text()='Edytuj']" # XPATH
+submit_edit_employee_button = "//button[text()='Zapisz']" # XPATH
